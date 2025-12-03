b1478c5ebe976065e4f7643ec98eeaacf38e58dc
rajashree <rajashree@udmglobal.com>
2025-12-03 20:35:59 +0530

    optimize

--- 11.bubble_sort/bubble_sort.js
+++ 11.bubble_sort/bubble_sort.js
@@ -1,18 +1,16 @@
 
 function bubbleSort(arr) {
- 
-  for(let i = arr.length; i > 0; i--) {
+  for (let i = arr.length; i > 0; i--) {
      let swaped = false;
-    for(let j = 0; j < i - 1; j++) {
+    for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j+1]) {
         [arr[j+1], arr[j]] = [arr[j], arr[j+1]];
-        swaps = true;
+        swaped = true;
       }
-     
     }
 
-    if(!swaps) {
-        break;
+    if (!swaped) {
+      break;
     }
   }
   return arr;
